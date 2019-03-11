cc031b532708933511f087f95afbe72c0b4581bf
Naoya-tech <naoya.techwork@gmail.com>
2019-03-11 10:38:17 +0900

    投稿ページの追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,26 @@ class ArticlesController < ApplicationController
 
 
 
+  def new
+    @article = Article.new
+  end
+
+
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
 
 
 
 
+    private
 
-
-
-
+  def article_params # ストロングパラメータを定義する
+    params.require(:article).permit(:title, :body)
+  end
 
 
 
